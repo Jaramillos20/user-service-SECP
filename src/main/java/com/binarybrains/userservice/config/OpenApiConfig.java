c207535628052ec7e846e97f7846914ff4d6808d
bryancsinformatica <bryancsinformatica@gmail.com>
2025-10-20 21:58:44 -0600

    doc: add OpenApiConfig.java

---
+++ src/main/java/com/binarybrains/userservice/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.binarybrains.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+     @Bean
+    public OpenAPI userServiceOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("User Service API")
+                .description("This API handles user management operations for the scalable e-commerce platform.")
+                .version("1.0.0")
+                .contact(new Contact()
+                    .name("untalbry")
+                    .url("https://github.com/untalbry")
+                )
+            );
+    }
+}
