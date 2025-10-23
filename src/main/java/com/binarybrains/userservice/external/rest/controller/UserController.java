b3f8b1c57d8546af5a596c28e0f5f40e2378078d
bryancsinformatica <bryancsinformatica@gmail.com>
2025-10-23 00:08:40 -0600

    doc: add swagger read user by id enpoint doc

--- src/main/java/com/binarybrains/userservice/external/rest/controller/UserController.java
+++ src/main/java/com/binarybrains/userservice/external/rest/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.binarybrains.userservice.external.rest.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,15 +10,40 @@ import org.springframework.web.bind.annotation.RestController;
 import com.binarybrains.userservice.core.buisness.input.UserService;
 import com.binarybrains.userservice.external.rest.dto.UserDto;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping(
+    value = "/user",
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE
+    )
 public class UserController {
     UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
     }
- 
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200", 
+            description = "User found successfully",
+            content = {
+                @Content(
+                schema = @Schema(implementation = UserDto.class)
+                )
+            }
+        ),
+        @ApiResponse(
+            responseCode = "404", 
+            description = "User not found",
+            content= @Content( schema= @Schema(example = "{\"error\": \"User not found\"}")) 
+        ),
+        @ApiResponse(responseCode = "500", description = "Internal server error", content = {@Content()})
+    })
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUsersById(@PathVariable("id") Integer id) {
         var result = userService.getById(id);
